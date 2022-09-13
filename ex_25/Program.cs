13366549756645be3e2bc2164fc772f3051dfb34
forbidden <forbidden@fedora.com>
2022-09-13 18:55:14 +0300

    add ex_25

---
+++ ex_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B. Решить задачу с использованием методов.
+int Pow(int x, int count)
+{   
+    int sum = 1;
+    while(count > 0)
+        {
+            sum = sum*x;
+            count--;   
+        }
+    return sum;
+}
+
+Console.WriteLine("Enter first int: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second int: ");
+int y = int.Parse(Console.ReadLine());
+
+int newpow = Pow(x, y);
+Console.WriteLine(newpow);
