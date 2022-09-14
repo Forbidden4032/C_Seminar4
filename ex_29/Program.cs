8edbf57814b7c8274868535cd40c160582219f6d
forbidden <forbidden@fedora.com>
2022-09-14 17:34:09 +0300

    add ex_29

---
+++ ex_29/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и 
+//выводит их на экран. Элементы массива вводятся пользователем.
+
+int[] array = new int[8];
+
+for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите числовой элемент №{i+1} и нажмите Enter: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+Console.WriteLine("Ввод элементов закончен, вывод на экран результата: ");
+    
+for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+
+
+
