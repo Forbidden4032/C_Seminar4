27350792bb9caf6f29dd80757d3ef56aaf93cfb1
forbidden <forbidden@fedora.com>
2022-09-14 16:34:48 +0300

    add ex_27

---
+++ ex_27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: " );
+int x = int.Parse(Console.ReadLine());
+
+int summ = 0;
+
+while(x > 0)
+    {
+        summ = summ + (x%10);
+        x = x / 10;
+    }
+Console.WriteLine($"Сумма чисел введенного числа равна {summ}");
+
+
+
+
+
+
+
